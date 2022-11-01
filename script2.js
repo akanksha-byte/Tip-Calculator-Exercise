043de5df4607d4220a4557b73eda3a1781331cf3
AKANKSHA MARSKOLE <54081676+akanksha-byte@users.noreply.github.com>
2022-11-01 16:06:22 +0000

    message is added with it

--- script2.js
+++ script2.js
@@ -34,33 +34,33 @@ const calculateBill = () => {
   const bill = Number(billInput.value)
 
   // get the tip from user & convert it into a percentage (divide by 100)
-  const tipPercentage = Number(tipInput.value)/100
-  
+  const tipPercentage = Number(tipInput.value) / 100
+
   // get the total tip amount
   const tipAmount = bill * tipPercentage
 
   // calculate the total (tip amount + bill)
   const total = tipAmount + bill
-  
+
 
   // calculate the per person total (total divided by number of people)
-const perPersonTotal = total/numberOfPeople
+  const perPersonTotal = total / numberOfPeople
 
   // update the perPersonTotal on DOM & show it to user
-perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
 }
 // .toLocaleString('en-US')
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
   // increment the amount of people
-numberOfPeople += 1
+  numberOfPeople += 1
 
   // update the DOM with the new number of people
-numberOfPeopleDiv.innerText = numberOfPeople
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-calculateBill()
+  calculateBill()
 }
 
 // ** Splits the bill between fewer people **
@@ -68,19 +68,22 @@ const decreasePeople = () => {
   // guard clause
   // if amount is 1 or less simply return
   // (a.k.a you can't decrease the number of people to 0 or negative!)
-if(numberOfPeople <=1){
-  alert('Hey! You cannot have less than 1 person!')
-  throw 'Hey! You cannot have less than 1 person!'
-  
-  return
-}
-  
+  if (numberOfPeople <= 1) {
+    alert('Hey! You cannot have less than 1 person!')
+    throw 'Hey! You cannot have less than 1 person!'
+
+    return ;
+  }
+
   // decrement the amount of people
-numberOfPeople -= 1
+  numberOfPeople -= 1
 
   // update the DOM with the new number of people
-numberOfPeopleDiv.innerText = numberOfPeople
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-calculateBill()
+  calculateBill()
 }
+
+
+// similarly create calculator
